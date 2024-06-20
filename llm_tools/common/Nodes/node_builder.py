031d0f99f30d695eafcf2939487102335c414942
LuisssDeAcero <114011536+LuisssDeAcero@users.noreply.github.com>
2024-06-20 11:56:45 -0600

    Set context node added

--- llm_tools/common/Nodes/node_builder.py
+++ llm_tools/common/Nodes/node_builder.py
@@ -9,12 +9,14 @@ from llm_tools.common.Nodes.llm_node import LLMNode
 from llm_tools.common.Nodes.read_file_node import ReadFileNode
 from llm_tools.common.Nodes.save_file_node import SaveFileNode
 from llm_tools.common.Nodes.parse_yaml import ParseYamlNode
+from llm_tools.common.Nodes.set_context_node import SetContextNode
 
 AVAILABLE_NODES: Dict[str, Type[NodeAbstract]] = {
     "LLM_NODE": LLMNode,
     "READ_FILE_NODE": ReadFileNode,
     "SAVE_FILE_NODE": SaveFileNode,
-    "PARSE_YAML_NODE": ParseYamlNode
+    "PARSE_YAML_NODE": ParseYamlNode,
+    "SET_CONTEXT_NODE": SetContextNode
 }
 
 
@@ -34,35 +36,5 @@ class AvailableNodeType(BaseModel):
         return value
 
     
-
-
-if __name__ == "__main__":
-    os.environ.setdefault("GOOGLE_APPLICATION_CREDENTIALS",
-                          "/Volumes/case-sensitive-volume/LLM_TOOLS/credentials/data-chat-credentials.json")
-    tickets = read_file("/Volumes/case-sensitive-volume/LLM_TOOLS/llm_tools/jira_tickets/data/output_yamls.txt")
-    _config = {
-        "max_iterations": 0,
-        "name": "END",
-        "type": "LLM_NODE",
-        "options": {
-            "llm_node_type": "GEMINI",
-            "next_node": "NO NODE",
-            "options": {
-                "system_prompt_template_name": "SI_GENERATE_CATEGORIES_PROMPT_TEMPLATE",
-                "system_prompt_input_values": {},
-                "prompt_template_name": "GENERATE_CATEGORIES_PROMPT_TEMPLATE",
-                "temperature": 0.0,
-                "options": {
-                    "project_id": "dea-analytics-prj-gemini",
-                    "region": "us-central1",
-                    "gcp_model_name": "gemini-1.5-pro-preview-0514"
-                }
-            }
-        }
-    }
-    _node = build_node(_config, None)
-    result = _node.process_input(node_input={"tickets": tickets})
-    print(result.next_node)
-    print(result.output_data)
 
 
---
+++ llm_tools/common/Nodes/set_context_node/__init__.py
@@ -0,0 +1,27 @@
+import os.path
+from typing import Any, Dict
+from pydantic import BaseModel, field_validator
+
+from llm_tools.common.Nodes import NodeAbstract, NodeConfig, NodeOutput, Graph
+from llm_tools.common import save_to_file
+
+
+class SetContextNodeConfig(BaseModel):
+    next_node: str
+    context_to_save: Dict[str, Any]
+
+
+class SetContextNode(NodeAbstract):
+    def __init__(self, config: NodeConfig, graph: Graph):
+        super().__init__(config, graph)
+        self.set_context_node_config: SetContextNodeConfig = SetContextNodeConfig(**self.config.options)
+
+    def process_input(self) -> NodeOutput:
+        for key, value in self.set_context_node_config.context_to_save.items():
+            self.graph.set_context(key, value)
+        print(f"Node {self.config.name} saved context vars:", *self.set_context_node_config.context_to_save.items())
+        return NodeOutput(
+            next_node=self.set_context_node_config.next_node
+        )
+
+
