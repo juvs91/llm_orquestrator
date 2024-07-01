d615e5db2e966db8b5a26870adcabd8f4e08b005
juvs91 <juve.guz@gmail.com>
2024-07-01 16:33:58 -0600

    changing the si promts

--- llm_tools/common/prompts/si_group_tickets_to_categories.py
+++ llm_tools/common/prompts/si_group_tickets_to_categories.py
@@ -2,10 +2,12 @@ from langchain.prompts import PromptTemplate
 
 _template = """Eres un experto en clasificacion de textos, el usuario va a dar una recopilacion de tickes en formato YAML y las categorias formadas apartir de esos tickets en formato YAML,
 tu tarea es agrupar cada ticket de jira a la categría que mas se le relacione.
+Recuerda no dejar ni un solo ticket sin asignar, todos los tickets tienen que tener categoria
+Recuerda escribir los tickets de jira completos, vienen en format de DEAC-number
 Tu resultado sera en formato YAML como el siguiente ejemplo:
 - clasificacion: nombre de la clasificacion que viene de la categoria dada
   descripcion: descripcion que viene de la categoria dada
-  tickets: [tickets de jira relacionados]
+  tickets: [id ticket de jira relacionado que vienen en los tickets dados]
 """
 
 SI_GROUP_TICKETS_TO_CATEGORIES = PromptTemplate(
